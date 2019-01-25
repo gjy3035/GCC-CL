2ec391e14471297245e410e8acc425c672a978ee
Elin24 <elonlin24@163.com>
2019-01-25 08:14:41 +0800

    add two new plugin-in for GCC-Collector

---
+++ noVehicle/noVehicle/src/script.cpp
@@ -0,0 +1,24 @@
+/*
+	THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
+				http://dev-c.com			
+			(C) Alexander Blade 2015
+*/
+
+/*
+	F4					activate
+	NUM2/8/4/6			navigate thru the menus and lists (numlock must be on)
+	NUM5 				select
+	NUM0/BACKSPACE/F4 	back
+	NUM9/3 				use vehicle boost when active
+	NUM+ 				use vehicle rockets when active
+*/
+
+#include "script.h"
+
+void ScriptMain()
+{
+	while (true) {
+		VEHICLE::SET_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(0.0f);
+		WAIT(0);
+	}
+}
